b2b05dfd4b3234a74387ebdb3b0d541203920459
Joel Callapa <joelcpd77@gmail.com>
2022-12-28 15:15:03 +0100

    validation paramethers

--- add.php
+++ add.php
@@ -1,14 +1,26 @@
 <?php 
   require "database.php";
-
+  
+  $error = null;
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-    $phoneNumber = $_POST["phone_number"];
+    if(empty($_POST["name"]) || empty($_POST["phone_number"])){
+      $error = "Rellena todo gil.";
+    } else if(strlen($_POST["phone_number"]) <9){
+      $error = "El numero debe ser almenos de 9 numeros.";
+    } else if(!is_numeric($_POST["phone_number"])){
+      $error = "Pon un numero gil";
+    }else{
+      $name = $_POST["name"];
+      $phoneNumber = $_POST["phone_number"];
    
-    $statement = $conn->prepare("INSERT INTO contacts (name, phone_number) VALUES ('$name','$phoneNumber')");
-    $statement->execute();
+      $statement = $conn->prepare("INSERT INTO contacts (name, phone_number) VALUES (:name, :phone_number)");
+      $statement->bindParam(":name", $_POST["name"]);
+      $statement->bindParam(":phone_number", $_POST["phone_number"]);
+      $statement->execute();
 
-    header("Location: index.php");
+      header("Location: index.php");
+    }
+    
   }
 ?>
 
@@ -77,6 +89,11 @@
           <div class="card">
             <div class="card-header">Add New Contact</div>
             <div class="card-body">
+              <?php if ($error): ?>
+                <p class = "text-danger">
+                  <?= $error ?>
+                </p>
+              <?php endif ?>
               <form method="POST" action="add.php">
                 <div class="mb-3 row">
                   <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
